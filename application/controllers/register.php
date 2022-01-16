f1de276c1aab7da3a79556325d5909c2f5d146b2
Aamir Sofi <ksaak33@gmail.com>
2022-01-16 11:58:56 +0530

    Fixed the date issue, password encry and organised database call

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,36 +1,50 @@
 <!-- Register Page Controller -->
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Register extends CI_Controller {
-    function __construct(){
+class Register extends CI_Controller
+{
+    function __construct()
+    {
         parent::__construct();
         $this->load->database();
         $this->load->model('register_model');
+        $this->load->helper('url');
     }
 
-	public function index(){
+    public function index()
+    {
+
+        echo date('y-m-d');
+        echo "<br>";
+        echo date('Y-m-d');
+        echo "<br>";
+        echo date('D, Y-M-d H:m:s');
+        // Store input data
         $inputPostData = [
             'u_id' => NULL,
             'u_name' => $this->input->post('name'),
             'u_email' => $this->input->post('email'),
             'u_phone' => $this->input->post('phone'),
             'u_username' => $this->input->post('username'),
-            'u_password' => $this->input->post('password'),
-            'u_doc' => date('d-m-Y H:m:s'),
-            'u_dou' => date('d-m-Y H:m:s'),
+            'u_password' => md5($this->input->post('password')),
+            'u_doc' => date('Y-m-d H:m:s'),
+            'u_dou' => date('Y-m-d H:m:s'),
             'u_status' => 1
         ];
-
+        // Prepare data for view
         $data['info'] = (object) $inputPostData; //sent to value attributes
-        $this->load->view('register',$data);
-        // echo "<pre>";
-        // print_r($inputPostData);
-        // echo "</pre>";
-        
-        $this->register_model->insertDb($inputPostData);
-        // $data['users'] = $this->register_model->getDbData();
-        // print_r($data['users']);
+
+        if ($this->input->post('register') == 1) {
+
+            $this->register_model->insertDb($inputPostData);
+
+            $this->load->view('register', $data);
+            // redirect('/index');
+        } else {
+            // Load the register form.
+            $this->load->view('register', $data);
+        }
     }
 
     // public function getDbData(){
@@ -40,3 +54,22 @@ class Register extends CI_Controller {
     //     echo "</pre>";
     // }
 }
+
+
+/**
+ * Get reques ->
+ *  if get request then 
+ *      display form/view
+ *      exit 
+ *  if post request then
+ *      - Prepare data
+ *      - Validate data
+ *      - if valdatation success
+ *          - Then database call
+ *              if(sucess)
+ *                  - then display view with sucess message
+ *              else
+ *                  - display view with error message.
+ *      - if validation fails
+ *          - Display view again with error message.
+ */
