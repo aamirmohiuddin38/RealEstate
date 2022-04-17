942dd8f47df8d71395f64ee80494756e2ca66ba6
Aamir Sofi <eraamirsofi@gmail.com>
2022-04-17 20:48:06 +0530

    Home + Contact + About Us

---
+++ application/views/front/contactus.php
@@ -0,0 +1,229 @@
+<link rel='stylesheet' id='elementor-post-16753-css' href='<?php echo base_url('houzez_assets/'); ?>wp-content/uploads/elementor/css/post-16941.css' type='text/css' media='all' />
+<section class="content-wrap">
+  <div data-elementor-type="wp-page" data-elementor-id="16941" class="elementor elementor-16941" data-elementor-settings="[]">
+    <div class="elementor-inner">
+      <div class="elementor-section-wrap">
+        <section class="elementor-section elementor-top-section elementor-element elementor-element-125aa3d elementor-section-height-min-height animated-slow elementor-section-boxed elementor-section-height-default elementor-section-items-middle animated fadeIn" data-id="125aa3d" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;,&quot;animation&quot;:&quot;fadeIn&quot;}">
+          <div class="elementor-background-overlay"></div>
+          <div class="elementor-container elementor-column-gap-default">
+            <div class="elementor-row">
+              <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-3b3a0df" data-id="3b3a0df" data-element_type="column">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-1bdf606 elementor-widget elementor-widget-heading" data-id="1bdf606" data-element_type="widget" data-widget_type="heading.default">
+                      <div class="elementor-widget-container">
+                        <h1 class="elementor-heading-title elementor-size-default">Contact us</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="elementor-section elementor-top-section elementor-element elementor-element-5ec2734 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="5ec2734" data-element_type="section">
+          <div class="elementor-container elementor-column-gap-default">
+            <div class="elementor-row">
+              <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-e1bd41f" data-id="e1bd41f" data-element_type="column">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-49cbafe elementor-widget elementor-widget-houzez_elementor_space" data-id="49cbafe" data-element_type="widget" data-widget_type="houzez_elementor_space.default">
+                      <div class="elementor-widget-container">
+                        <div class="houzez-spacer">
+                          <div class="houzez-spacer-inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="elementor-section elementor-top-section elementor-element elementor-element-d0b02e7 elementor-section-content-top animated-slow elementor-section-boxed elementor-section-height-default elementor-section-height-default animated fadeIn" data-id="d0b02e7" data-element_type="section" data-settings="{&quot;animation&quot;:&quot;fadeIn&quot;}">
+          <div class="elementor-container elementor-column-gap-default">
+            <div class="elementor-row">
+              <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-3139327" data-id="3139327" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-8ff1404 elementor-widget elementor-widget-text-editor" data-id="8ff1404" data-element_type="widget" data-widget_type="text-editor.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-text-editor elementor-clearfix">
+                          <p>Write your quries and we will reach you as soon as possible.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="elementor-element elementor-element-f4a5b85 elementor-button-align-stretch elementor-widget elementor-widget-houzez_elementor_contact_form" data-id="f4a5b85" data-element_type="widget" data-widget_type="houzez_elementor_contact_form.default">
+                      <div class="elementor-widget-container">
+
+                        <script type="application/javascript">
+                          jQuery(document).bind("ready", function() {
+                            houzezValidateElementor("#houzez-form-f4a5b85");
+                          });
+                        </script>
+
+                        <form class="elementor-form" id="houzez-form-f4a5b85" method="post" name="New Form" action="<?php echo base_url('houzez/save_message'); ?>" novalidate="novalidate">
+                          <div class="elementor-form-fields-wrapper elementor-labels-above">
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-name elementor-col-50 elementor-field-required elementor-mark-required">
+                              <label for="form-field-name" class="elementor-field-label">First Name</label><input type="text" name="first_name" id="form-field-name" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Enter your name" title="* First Name" required="required">
+                            </div>
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-bf18955 elementor-col-50 elementor-field-required elementor-mark-required">
+                              <label for="form-field-bf18955" class="elementor-field-label">Last Name</label><input type="text" name="last_name" id="form-field-bf18955" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Enter your last name" title="* Last Name" required="required">
+                            </div>
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-bf189551 elementor-col-50 elementor-field-required elementor-mark-required">
+                              <label for="form-field-bf189551" class="elementor-field-label">Phone Number</label><input type="text" name="f_phone" id="form-field-bf189551" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Enter your last name" title="* Phone Number" required="required">
+                            </div>
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-email elementor-col-50 elementor-field-required elementor-mark-required">
+                              <label for="form-field-email" class="elementor-field-label">Email</label><input type="email" name="email" id="form-field-email" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Enter your Email" title="* Email" required="required">
+                            </div>
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-bf189552 elementor-col-100 elementor-field-required elementor-mark-required">
+                              <label for="form-field-bf189552" class="elementor-field-label">Subject</label><input type="text" name="f_subject" id="form-field-bf189552" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Enter your subject" title="* Subject" required="required">
+                            </div>
+
+                            <div class="elementor-field-group elementor-column form-group elementor-field-group-message elementor-col-100 elementor-field-required elementor-mark-required">
+                              <label for="form-field-message" class="elementor-field-label">Message</label><textarea class="elementor-field-textual elementor-field elementor-size-sm" name="message" id="form-field-message" rows="4" placeholder="Message" required="required" title="* Message"></textarea>
+                            </div>
+
+                            <div class="elementor-field-group elementor-column elementor-field-type-submit elementor-col-100">
+                              <button type="submit" class="btn houzez-submit-button houzez-contact-form-js elementor-button elementor-size-sm">
+                                <i class="btn-loader houzez-loader-js"></i>
+                                Submit </button>
+                            </div>
+                          </div><!-- End wrapper-->
+                          <br>
+                          <div class="ele-form-messages"></div>
+                          <div class="error-container"></div>
+
+                        </form>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="elementor-column elementor-col-25 elementor-top-column elementor-element elementor-element-bd2c54b" data-id="bd2c54b" data-element_type="column">
+                <div class="elementor-column-wrap">
+                  <div class="elementor-widget-wrap">
+                  </div>
+                </div>
+              </div>
+              <div class="elementor-column elementor-col-25 elementor-top-column elementor-element elementor-element-66ddd7d" data-id="66ddd7d" data-element_type="column">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-23057bd elementor-widget elementor-widget-text-editor" data-id="23057bd" data-element_type="widget" data-widget_type="text-editor.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-text-editor elementor-clearfix">
+                          <p class="padding-top"><strong>For inquiries</strong><strong>&nbsp;contact:</strong></p>
+                          <!-- <p><strong>Azhar Hussain</strong><br>CEO<br>Lambert Lane LalChowk, Srinagar<br>9622806016<br>alazhar1619@gmail.com</p> -->
+                          <p><strong>Faisal Khan</strong><br>Founder<br>Lambert Lane LalChowk, Srinagar<br>7006382025<br>faisyalkhan@gmail.com</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="elementor-element elementor-element-3c1041e elementor-widget elementor-widget-text-editor" data-id="3c1041e" data-element_type="widget" data-widget_type="text-editor.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-text-editor elementor-clearfix">
+                          <p class="padding-top"><strong>Corporate
+                              Headquarters</strong><br>Lambert Lane LalChowk, Srinagar</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="elementor-element elementor-element-0b2a1b4 e-grid-align-left elementor-shape-rounded elementor-grid-0 elementor-widget elementor-widget-social-icons" data-id="0b2a1b4" data-element_type="widget" data-widget_type="social-icons.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-social-icons-wrapper elementor-grid">
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-facebook elementor-animation-shrink elementor-repeater-item-4d4e750" href="#" target="_blank">
+                              <span class="elementor-screen-only">Facebook</span>
+                              <i class="fab fa-facebook"></i> </a>
+                          </div>
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-twitter elementor-animation-shrink elementor-repeater-item-a2d8d9c" href="#" target="_blank">
+                              <span class="elementor-screen-only">Twitter</span>
+                              <i class="fab fa-twitter"></i> </a>
+                          </div>
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-youtube elementor-animation-shrink elementor-repeater-item-4483b35" href="#" target="_blank">
+                              <span class="elementor-screen-only">Youtube</span>
+                              <i class="fab fa-youtube"></i> </a>
+                          </div>
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-linkedin elementor-animation-shrink elementor-repeater-item-4cd5d30" href="#" target="_blank">
+                              <span class="elementor-screen-only">Linkedin</span>
+                              <i class="fab fa-linkedin"></i> </a>
+                          </div>
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-instagram elementor-animation-shrink elementor-repeater-item-9e48d2c" href="#" target="_blank">
+                              <span class="elementor-screen-only">Instagram</span>
+                              <i class="fab fa-instagram"></i> </a>
+                          </div>
+                          <div class="elementor-grid-item">
+                            <a class="elementor-icon elementor-social-icon elementor-social-icon-skype elementor-animation-shrink elementor-repeater-item-a996753" href="#" target="_blank">
+                              <span class="elementor-screen-only">Skype</span>
+                              <i class="fab fa-skype"></i> </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <!-- Google Map -->
+        <?php /* <section class="elementor-section elementor-top-section elementor-element elementor-element-7c709d6 animated-slow elementor-section-boxed elementor-section-height-default elementor-section-height-default animated fadeIn" data-id="7c709d6" data-element_type="section" data-settings="{&quot;animation&quot;:&quot;fadeIn&quot;}">
+          <div class="elementor-container elementor-column-gap-default">
+            <div class="elementor-row">
+              <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-6a68845" data-id="6a68845" data-element_type="column">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-6550704 elementor-widget elementor-widget-google_maps" data-id="6550704" data-element_type="widget" data-widget_type="google_maps.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-custom-embed">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d715.4159210645155!2d74.81615842917545!3d34.07210810966925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e18f9a19e7d441%3A0x55965367b2d050e1!2sLambert%20Lane%2C%20Srinagar%20190001!5e1!3m2!1sen!2sin!4v1637491122319!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="elementor-element elementor-element-81258c8 elementor-widget elementor-widget-text-editor" data-id="81258c8" data-element_type="widget" data-widget_type="text-editor.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-text-editor elementor-clearfix">
+                          <p style="text-align: left;"><strong>Address</strong>: Lambert Lane Lalachowk, Srinagar
+                            – <a href="https://www.google.com/maps/dir//Lambert+Lane,+Srinagar+190001/@34.072107,74.8145169,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x38e18f9a19e7d441:0x55965367b2d050e1!2m2!1d74.8167056!2d34.072107" target="_BLANK">Get Directions</a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        */ ?>
+        <section class="elementor-section elementor-top-section elementor-element elementor-element-ddbf539 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="ddbf539" data-element_type="section">
+          <div class="elementor-container elementor-column-gap-default">
+            <div class="elementor-row">
+              <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-84f5425" data-id="84f5425" data-element_type="column">
+                <div class="elementor-column-wrap elementor-element-populated">
+                  <div class="elementor-widget-wrap">
+                    <div class="elementor-element elementor-element-11366a9 elementor-widget elementor-widget-spacer" data-id="11366a9" data-element_type="widget" data-widget_type="spacer.default">
+                      <div class="elementor-widget-container">
+                        <div class="elementor-spacer">
+                          <div class="elementor-spacer-inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ application/views/front/home.php
@@ -0,0 +1,40 @@
+	<?php $this->load->view('front/parts/home_slider_part'); ?>
+	<?php $this->load->view('front/parts/advanced_search_part'); ?>
+
+	<section class="content-wrap">
+		<div data-elementor-type="wp-page" data-elementor-id="16753" class="elementor elementor-16753" data-elementor-settings="[]">
+			<div class="elementor-inner">
+				<div class="elementor-section-wrap">
+					<!-- Welcome & Services -->
+					<?php $this->load->view('front/parts/welcome_services_part'); ?>
+
+					<!-- Featured listing -->
+					<?php $this->load->view('front/parts/featured_listing_part'); ?>
+
+					<!-- Why Hyunkunun is perfect choice -->
+					<?php // $this->load->view('front/parts/perfect_choice_part'); 
+					?>
+
+					<!-- Explore lifestyle -->
+					<?php //$this->load->view('front/parts/explore_lifestyle_part'); 
+					?>
+
+					<!-- Properties for sale -->
+					<?php $this->load->view('front/parts/properties_for_sale_part'); ?>
+
+					<!-- Explore cities -->
+					<?php //  $this->load->view('front/parts/explore_cities_part'); 
+					?>
+
+					<!-- Get in touch with us -->
+					<?php // $this->load->view('front/parts/get_in_touch_part'); 
+					?>
+
+					<!-- Brands -->
+					<?php // $this->load->view('front/parts/brands_part'); 
+					?>
+
+				</div>
+			</div>
+		</div>
+	</section>
