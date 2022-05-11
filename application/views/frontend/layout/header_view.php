0dddcbec57133b44bac78b872be14f4507e11b99
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-05-11 20:49:47 +0530

    added timestamp to linked css - bug fix

--- application/views/frontend/layout/header_view.php
+++ application/views/frontend/layout/header_view.php
@@ -8,21 +8,20 @@
   <title>HomeSphere:Realestate</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <!--Bootstrap -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/fontawesome6/css/all.min.css"> <!-- FontAwesome -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/navbar_view.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/main.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/welcome.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/grid_view.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property_sale_view.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick.css"> <!-- Slick Css- -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick-theme.css"> <!-- Slick-->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/footer.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/about.css"> <!-- About Us -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/fontawesome6/css/all.min.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- FontAwesome -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/navbar_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/main.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/welcome.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/grid_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property_sale_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick Css- -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick-theme.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick-->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/footer.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <!-- Contact Us -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/contact.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/contact.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <!-- About Us -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/about.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/about.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <script type='text/javascript' src="<?php echo base_url(); ?>front_assets/js/map.js"></script> <!-- Google Map JS -->
 </head>
 
