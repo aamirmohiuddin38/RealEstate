00804dea9b25026813fbccf9d43d74b09c239801
Zaid Javeed <erzaidJaveed@gmail.com>
2022-05-11 18:30:14 +0530

    dynamic images added to property sale

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -8,13 +8,14 @@ class Front extends CI_Controller
     {
         parent::__construct();
         $this->load->helper('url');
-        $this->load->model(array('front_model'));
+        $this->load->model(array('front_model', 'property_model'));
     }
 
     public function index()
     {
         $data = [];
         $data['property_sale'] = $this->front_model->property_sale();
+        $data['images'] = $this->property_model->front_img_list();
         $data['content'] = $this->load->view('frontend/pages/home', $data, true);
         $this->load->view('frontend/layout/main_wrapper', $data);
     }
@@ -28,5 +29,4 @@ class Front extends CI_Controller
         $data['content'] = $this->load->view('frontend/pages/contact_view', '', true);
         $this->load->view('frontend/layout/main_wrapper', $data);
     }
-
 }
