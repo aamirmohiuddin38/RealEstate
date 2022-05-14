4610646daf6720ad8a70723ca95e16e5e4e63d7c
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-05-14 21:52:01 +0530

    contact us updated

--- application/views/frontend/pages/contact_view.php
+++ application/views/frontend/pages/contact_view.php
@@ -1,25 +1,9 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>About Us</title>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous"
-    />
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="contact.css" rel="stylesheet" />
-  </head>
-  <body> -->
 <div class="card_contact">
   <img src="https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="image" class="image_contact" />
+  <div class="head_text_contact">Contact Us</div>
 </div>
-<div class="head_text_contact">Contact Us</div>
-<div class="container d-flex">
+
+<div class="container">
   <div class="wrapper-left">
     <div class="row mt-5 mb-3 text-info text-center">
       <div class="col-md-12">
@@ -68,7 +52,7 @@
       </div>
     </form>
   </div>
-  <div class="right">
+  <!-- <div class="right">
     <div class="row">
       <div class="col-md-12">
         <p class="padding-top"><strong>For inquiries</strong><strong>&nbsp;contact:</strong></p>
@@ -92,7 +76,7 @@
         <button type="button" class="btn btn-danger"><i class="fa-brands fa-linkedin"></i></button>
       </div>
     </div>
-  </div>
+  </div> -->
 </div>
 <!-- </body>
 </html> -->
