2079455cc405bf77d839fd236dc69288cdbd8315
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-05-23 22:29:27 +0530

    Property Sale Slider Fixed

--- application/views/frontend/layout/header_view.php
+++ application/views/frontend/layout/header_view.php
@@ -12,13 +12,13 @@
   <link rel="shortcut icon" href="<?php echo base_url() . $setting->favicon; ?>" type="image/x-icon">
   <!-- font Awesome -->
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/fontawesome6/css/all.min.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- FontAwesome -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick Css- -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick-theme.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick-->
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/navbar_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/main.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/welcome.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/grid_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property_sale_view.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick Css- -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/slick-theme.css?<?php echo date('l jS \of F Y h:i:s A'); ?>"> <!-- Slick-->
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/property.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/footer.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>front_assets/css/search.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
