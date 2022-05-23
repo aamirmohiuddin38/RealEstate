22d02722ec7427f4b56f2d0cc9ed9902493310c3
AamirMohiuddin38 <eramirmohiuddin@gmail.com>
2022-05-24 00:52:01 +0530

    Newly added buttons position changed and icon fix

--- application/views/frontend/layout/navbar_view.php
+++ application/views/frontend/layout/navbar_view.php
@@ -77,21 +77,24 @@
 				<li class="nav-item">
 					<a class="nav-link " href="<?php echo base_url('index.php/front/contactUs'); ?>">Contact</a>
 				</li>
-				<li class="nav-item">
-				    <a class="nav-link" href="<?php echo base_url('index.php/front/'); ?>#searchSection"><i class="fa-solid fa-magnifying-glass mr-1"></i>Search</a>
-				</li>
-				<li class="nav-item">
-				<a class="nav-link" href="<?php echo base_url('index.php/front/'); ?>#welcomeSection">Why Us?</a>
-				</li>
 			</ul>
+
+			<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="<?php echo base_url('index.php/front/'); ?>#searchSection"><i class="fa-solid fa-magnifying-glass mr-1"></i>Search</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="<?php echo base_url('index.php/front/'); ?>#welcomeSection">Why Us?</a>
+			</li>
+				</ul>
 
 			<!-- login -->
 			<?php if (!$this->session->userdata('isLogIn')) { ?>
-				<a href="<?php echo base_url('index.php/login'); ?>"><button class="btn btn-info btn-sm" title="Please Login">LogIn</button></a>
+				<a href="<?php echo base_url('index.php/login'); ?>"><button class="btn btn-info btn-sm ml-2" title="Please Login">LogIn</button></a>
 			<?php } ?>
 			<!-- logout -->
 			<?php if ($this->session->userdata('isLogIn')) { ?>
-				<a href="<?php echo base_url('index.php/logout'); ?>"><button class="btn btn-info btn-sm" title="<?php echo $this->session->userdata('fullname'); ?>">Logout</button></a>
+				<a href="<?php echo base_url('index.php/logout'); ?>"><button class="btn btn-info btn-sm ml-2" title="<?php echo $this->session->userdata('fullname'); ?>">Logout</button></a>
 
 			<?php } ?>
 
